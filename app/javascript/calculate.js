c2854371fd1c0399bfe767cf022048ce372c6a84
hirowood <hirowoodandmika730@gmail.com>
2025-04-15 11:01:30 +0900

    少し修正

--- app/javascript/calculate.js
+++ app/javascript/calculate.js
@@ -2,13 +2,11 @@
 window.addEventListener('turbo:render', () => {
   const SALES_COMMISSION_PRICE = 0.9
   const price_input = document.querySelector("#item-price");
-  const tax_price = document.querySelector("#add-tax-price");
-  const profit = document.querySelector('#profit')
   price_input.addEventListener('input', () => {
     let price = Number(price_input.value)
     let sales = Math.floor(price  * SALES_COMMISSION_PRICE);
     let price_commission = price - sales
-    tax_price.textContent =price_commission;
-    profit.textContent = sales;
+    document.querySelector("#add-tax-price").textContent = price_commission;
+    document.querySelector('#profit').textContent = sales;
   });
 });
