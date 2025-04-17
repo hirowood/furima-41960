cb3f679981ffd24ca8659023d4a089108fb1ef62
hirowood <hirowoodandmika730@gmail.com>
2025-04-17 10:13:12 +0900

    ダブルクォーテーションやり忘れ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
   before_action :set_item, only: [:show, :edit]
   def index
-    @items = Item.includes(:user).order(created_at: DESC)
+    @items = Item.includes(:user).order(created_at: 'DESC')
   end
 
   def new
