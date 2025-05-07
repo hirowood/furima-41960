3b0153976d853944b22c86ca9757318e76be6e71
hirowood <hirowoodandmika730@gmail.com>
2025-05-07 13:12:52 +0900

    commit

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,7 +6,7 @@ bundle install
 bundle exec rake assets:clobber
 bundle exec rake assets:precompile
 
-bundle exec rake db:migrateDISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+
 
 
 
